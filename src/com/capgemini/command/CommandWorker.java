1cb1ed4b6b905e52ab28d5462a622b1704832882
ValentinDupitier <valentindupitier@gmail.com>
2018-08-29 16:15:58 +0200

    get previous function

--- src/com/capgemini/command/CommandWorker.java
+++ src/com/capgemini/command/CommandWorker.java
@@ -3,14 +3,26 @@ package com.capgemini.command;
 import java.util.ArrayList;
 
 public class CommandWorker {
-	private ArrayList <Command> commandList =  new ArrayList <Command>();
-	
+	private ArrayList<Command> commandList = new List<Command>();
+
 	public void addFunction(Command commandToAdd) {
 		this.commandList.add(commandToAdd);
 	}
-	
+
 	public int getCurrentIndex(Command command) {
 		int result = this.commandList.indexOf(command);
 		return result;
 	}
-} 
+
+	public Command getPrevious(Command currentCommand) {
+		Command commandResult;
+		int currentCommandIndex = getCurrentIndex(currentCommand);
+		if (currentCommandIndex==0) {
+			commandResult = this.commandList.get(commandList.size()-1);
+		}else {
+			commandResult = this.commandList.get(currentCommandIndex-1);
+		}
+		return commandResult;
+
+	}
+}
