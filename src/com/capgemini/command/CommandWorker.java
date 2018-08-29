81588ff339e439c15cc6c38f1df9e19f3c9548d8
MathieuRevereau <mathieu.revereau@gmail.com>
2018-08-29 15:27:19 +0200

    function getCurrentIndex

--- src/com/capgemini/command/CommandWorker.java
+++ src/com/capgemini/command/CommandWorker.java
@@ -3,9 +3,14 @@ package com.capgemini.command;
 import java.util.ArrayList;
 
 public class CommandWorker {
-	private ArrayList <Command> functionsList =  new ArrayList <Command>();
+	private ArrayList <Command> commandList =  new ArrayList <Command>();
 	
 	public void addFunction(Command commandToAdd) {
-		this.functionsList.add(commandToAdd);
+		this.commandList.add(commandToAdd);
+	}
+	
+	public int getCurrentIndex(Command command) {
+		int result = this.commandList.indexOf(command);
+		return result;
 	}
 } 
